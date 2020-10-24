bbb1de37d576912d2d107e26cbe5820fa22573fb
PedroMQP <manolopantufla@hotmail.com>
2020-10-23 19:51:06 -0500

    Modelovectorial inconcluso

---
+++ ModeloVectorial.py
@@ -0,0 +1,33 @@
+import numpy as np #Se utiliza numpy para crear arreglos ya que estos son más rapidos que las listas
+class ModeloVectorial:
+
+	#El vocabulario debe ser una lista en el orden que se desee que esten las caracteristicas en los vectores
+	def __init__(self,rowSize,vocabulary):
+		self.vectors = []
+		self.vocabulary = {}
+		iniVoc(vocabulary)
+	def __init__(self,lists,vocabulary):
+		self.vocabulary = {}
+		self.vectors = np.array(lists)
+		iniVoc(vocabulary)
+	def iniVoc(self,vocabulary):
+		self.tamVoc = len(vocabulary)
+		#Iniciamos el diccionario con las unidades del vocabulario como clave y su ubicación en el vector como valor
+		for i in range(len(vocabulary)):
+			self.vocabulary[vocabulary[i]] = i
+
+	def addVector(self,lista):# Agregamos un nuevo vector nuestro espacio vectorial
+		nv = np.array([lista])
+		try:
+			self.vectors = np.concatene(self.vectors,nv)
+		except:#El espacio vectorial esta vacio
+			self.vectors = nv
+
+	def contLemmas(self,lista):#Formamos la lista con la concordancia de cada lemma
+		vector = [0]*tamVoc
+		for lemma in lista:
+			vector[vocabulary[lemma]] = vector[vocabulary[lemma]] + 1
+		return vector
+
+
+
